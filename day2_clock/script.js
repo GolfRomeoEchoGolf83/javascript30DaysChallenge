786b27d306fa73f201c9c20a3c400643f984d57b
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-12 12:06:27 +0100

    Generating minute hand

--- day2_clock/script.js
+++ day2_clock/script.js
@@ -1,13 +1,25 @@
 /**
  * Created by Greg on 12/12/2016.
  */
+
+const secondHand = document.querySelector('.second-hand'); // grabbing the second's hand class
+const minsHand = document.querySelector('.min-hand'); // grabbing the minute's hand'
+
 function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    /* getting seconds of now */
-    const secondsDegrees = ((seconds / 60) * 360);
-    /* transforming seconds into angles */
+
+    const seconds = now.getSeconds(); //  getting seconds of now
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // transforming seconds into angles !! Adding +90 to offsetting css .hand transform: rotate(90deg)//
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60 ) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegree}deg)`;
+
 
 
 }
+
 setInterval(setDate, 1000);
+
+setDate();
