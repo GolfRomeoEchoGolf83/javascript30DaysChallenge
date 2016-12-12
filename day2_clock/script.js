3b238d6db245a410065da12b10ce5a8e344cdb83
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-12 11:36:19 +0100

    Converting seconds into degrees

--- day2_clock/script.js
+++ day2_clock/script.js
@@ -2,6 +2,12 @@
  * Created by Greg on 12/12/2016.
  */
 function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    /* getting seconds of now */
+    const secondsDegrees = ((seconds / 60) * 360);
+    /* transforming seconds into angles */
+
 
 }
 setInterval(setDate, 1000);
