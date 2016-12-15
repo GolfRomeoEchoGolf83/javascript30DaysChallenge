2b5665305a2380e821f58c6b18e60375d8c2c2ad
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-15 14:12:26 +0100

    When transition end, toggle open-active class

--- day5_flex_panel/script.js
+++ day5_flex_panel/script.js
@@ -3,9 +3,19 @@
  */
 const panels = document.querySelectorAll('.panel');
 
+/* for every panel clicked, find the function tooglePOpen */
 function toggleOpen() {
     this.classList.toggle('open');
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen))
-/* for every panel clicked, find the function tooglePOpen */
+/* when a panel transition ends, toggle .open-active to it */
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
