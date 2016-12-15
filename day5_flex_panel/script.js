99715a5557a3371b577f5a63c78f87376b8117e0
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-15 13:58:27 +0100

    When a panel is clicked, a function is called and toggle class open to .panel

--- day5_flex_panel/script.js
+++ day5_flex_panel/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by Greg on 14/12/2016.
  */
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+/* for every panel clicked, find the function tooglePOpen */
