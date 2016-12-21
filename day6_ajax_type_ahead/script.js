529e88f14ac2098f3e36f70ae66a4b15ab76b767
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-21 17:52:49 +0100

    Adding functions : - which displays matches - which convert string to number with comma
    Adding DOM manipulation

--- day6_ajax_type_ahead/script.js
+++ day6_ajax_type_ahead/script.js
@@ -10,6 +10,7 @@ fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
 
+// function which find matches between a word and a city or a state
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         // figuring out i the city matches what was searched
@@ -19,3 +20,32 @@ function findMatches(wordToMatch, cities) {
         return place.city.match(regex) || place.state.match(regex);
     });
 }
+
+// function which replace a string to a regex
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+// function which shows up cities or states which refers to the word searched
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        // returning a massive array
+        return `
+<li>
+<span class="name">${cityName}, ${stateName}</span>
+<span class="population">${numberWithCommas(place.population)}</span>
+</li>
+`;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
