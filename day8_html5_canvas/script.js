848775e43d5ab8ea07748286ff17a44d0464c177
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-12-24 13:56:06 +0100

    adapting canva to window size

--- day8_html5_canvas/script.js
+++ day8_html5_canvas/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Greg on 22/12/2016.
  */
+const canvas = document.querySelector('#draw');
+const context = canvas.getContext('2D'); // gets context of document
+
+// adapting canvas to window size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
