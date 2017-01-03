ae5013fe9e82e62d94bede9c3fdba845e9c5f588
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-03 16:35:27 +0100

    Factoring everything in order to get it operationnal

--- day8_html5_canvas/script.js
+++ day8_html5_canvas/script.js
@@ -2,7 +2,7 @@
  * Created by Greg on 22/12/2016.
  */
 const canvas = document.querySelector('#draw');
-const context = canvas.getContext('2D'); // gets context of document
+const context = canvas.getContext('2d'); // gets context of document
 
 // adapting canvas to window size
 canvas.width = window.innerWidth;
@@ -14,6 +14,9 @@ context.lineJoin = 'round';
 context.lineCap = 'round';
 context.lineWidth = 100;
 
+/*
+ variables
+ */
 
 // flag when click down drawning, click up not drawing
 let isDrawing = false;
@@ -27,29 +30,26 @@ let hue = 0;
 // line width with color
 let direction = true;
 
+/*
+ function
+ */
+
 // function which will draw
 function draw(e) {
     if (!isDrawing) return; // stop function from running when mouse up
     console.log(e);
     // playing with HSL with 100% S and 50% L
-    context.strokeStyle = `hsl(${hue},100%; 50%)`;
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     context.beginPath();
     // start from
     context.moveTo(lastX, lastY);
     // go to
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
-}
-
-// dealing with mouse action
-canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
-
-    // getting incrementation into coloring
     hue++;
 
-    // resetting hue
+// resetting hue
     if (hue >= 360) {
         hue = 0;
     }
@@ -63,8 +63,16 @@ canvas.addEventListener('mousedown', (e) => {
         context.lineWidth++;
     } else {
         context.lineWidth--;
-
     }
+}
+
+/*
+ actions
+ */
+// dealing with mouse action
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
 canvas.addEventListener('mousemove', draw);
