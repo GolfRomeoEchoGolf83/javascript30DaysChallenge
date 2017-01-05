9b7e693a8569e55391b79069df20e37da842332d
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-05 16:05:25 +0100

    Preparing files

---
+++ day9_dev_tools/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Greg on 05/01/2017.
+ */
+const dogs = [{name: 'Snickers', age: 2}, {name: 'hugo', age: 8}];
+function makeGreen() {
+    const p = document.querySelector('p');
+    p.style.color = '#BADA55';
+    p.style.fontSize = '50px';
+}
+
+
+// Regular
+// Interpolated
+// Styled
+// warning!
+// Error :|
+// Info
+// Testing
+// clearing
+// Viewing DOM Elements
+// Grouping together
+// counting
+// timing
