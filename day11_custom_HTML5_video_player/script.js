b0b10b577e9d91e4ce4b6dc6833ded4d40bf6f11
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-10 17:47:06 +0100

    Setting up all constantes which will be used

--- day11_custom_HTML5_video_player/script.js
+++ day11_custom_HTML5_video_player/script.js
@@ -3,6 +3,13 @@
  */
 
 /* Get elements on the page */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build functions */
 
