2f0701f5a1d904a3d9bdf91498de672cb6c43450
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-13 14:19:59 +0100

    Get info when window is scrolled

--- day_13_slide_in_on_scroll/script.js
+++ day_13_slide_in_on_scroll/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Greg on 13/01/2017.
  */
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    console.log(e);
+}
+
+window.addEventListener('scroll', checkSlide);
