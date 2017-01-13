16574d78a31219ab7eb2813c5987c6229af3685f
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-13 14:23:24 +0100

    Implementing debounce() function which call the checkSlide function every 20ms

--- day_13_slide_in_on_scroll/script.js
+++ day_13_slide_in_on_scroll/script.js
@@ -3,8 +3,23 @@
  */
 const sliderImages = document.querySelectorAll('.slide-in');
 
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
 function checkSlide(e) {
     console.count(e);
 }
 
-window.addEventListener('scroll', checkSlide);
+window.addEventListener('scroll', debounce(checkSlide));
