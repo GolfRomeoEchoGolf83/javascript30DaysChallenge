5f32d7179b4b7453dd605a5451f98b2bd55ab588
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-16 16:06:02 +0100

    dealing with a way to copy an object using assign() instead of using reference

--- day_14_js_references_vs_copying/script.js
+++ day_14_js_references_vs_copying/script.js
@@ -16,50 +16,59 @@
 //     console.log(name, name2);
 
 // Let's say we have an array
-const players = ['Greg', 'Sarah', 'Ryan', 'Pop'];
-
+// const players = ['Greg', 'Sarah', 'Ryan', 'Pop'];
+//
 // and we want to make a copy of it.
-const team = players;
-
-console.log(players, team);
-
+// const team = players;
+//
+// console.log(players, team);
+//
 // You might think we can just do something like this:
-team[3] = 'Lux';
-console.log(team);
-console.log(players);
-
+// team[3] = 'Lux';
+// console.log(team);
+// console.log(players);
+//
 // however what happens when we update that array?
-
+//
 // now here is the problem!
-
+//
 // oh no - we have edited the original array too!
-
+//
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
-
+//
 // So, how do we fix this? We take a copy instead!
-const team2 = players.slice();
-
+// const team2 = players.slice();
+//
 // one day
-
+//
 // or create a new array and concat the old one in
-const team3 = [].concat(players);
-
+// const team3 = [].concat(players);
+//
 // or use the new ES6 Spread
-const team4 = [...players];
-team4[3] = 'heeeeey hoooo';
-console.log(team4);
-
-const team5 = Array.from(players);
+// const team4 = [...players];
+// team4[3] = 'heeeeey hoooo';
+// console.log(team4);
+//
+// const team5 = Array.from(players);
 
 // now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
 
 // with Objects
+const person = {
+    name : "Greg Dcrx",
+    age: 32
+};
 
 // and think we make a copy:
+// const captain = person;
+// captain.number = 99;
 
 // how do we take a copy instead?
+const captain2 = Object.assign({}, person, {number: 99});
+console.log(captain2);
+console.log(person);
 
 // We will hopefully soon see the object ...spread
 
