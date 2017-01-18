7df43eb57e90709a4255824e28da998d1705952d
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-18 16:56:32 +0100

    setting up x and y with textbox containing h1

--- day_16_CSS_text_shadow_mouse_move_effect/script.js
+++ day_16_CSS_text_shadow_mouse_move_effect/script.js
@@ -7,12 +7,18 @@ const text = hero.querySelector('h1');
 
 function shadow(e) {
     // console.log(e);
+
     // const width = hero.offsetWidth;
     // const height = hero.offsetHeight;
     const {offsetWidth: width, offsetHeight: height} = hero;
     let {offsetX: x, offsetY: y} = e;
+    // console.log(x, y);
 
-    console.log(x, y);
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+    console.log(this, e.target);
 
 }
 
