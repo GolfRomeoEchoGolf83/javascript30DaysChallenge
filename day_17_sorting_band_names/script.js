e9b1cdc31a43093e280cd41975425a4efba83fca
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-19 17:23:44 +0100

    symplifying agan sort function of sortedBands

--- day_17_sorting_band_names/script.js
+++ day_17_sorting_band_names/script.js
@@ -9,8 +9,7 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 function strip(bandName) {
     return bandName.replace(/^(a |the |an )/i, '').trim();
 }
-const sortedBands = bands.sort((a, b) => {
-    return strip(a) > strip(b) ? 1 : -1;
-});
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
 
 console.log(sortedBands);
