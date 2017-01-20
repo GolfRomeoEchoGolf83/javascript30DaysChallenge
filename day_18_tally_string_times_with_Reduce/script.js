a57aa9484ec7457f7d6bbfcea237b7010d6c38f7
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-20 15:43:45 +0100

    converting array's data into float seconds

--- day_18_tally_string_times_with_Reduce/script.js
+++ day_18_tally_string_times_with_Reduce/script.js
@@ -3,6 +3,12 @@
  */
 const timeNodes = Array.from(document.querySelectorAll('[date-time]'));
 
+// converting array's data into time format
 const seconds = timeNodes
     .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;
+        console.log(mins, secs);
+    })
 
