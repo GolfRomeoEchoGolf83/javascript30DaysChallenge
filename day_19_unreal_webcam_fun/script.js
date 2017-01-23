539b589f206e619b5700a0d11be5a9d25dba535f
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-23 22:53:04 +0100

    setting up constantes

--- day_19_unreal_webcam_fun/script.js
+++ day_19_unreal_webcam_fun/script.js
@@ -1,3 +1,9 @@
 /**
  * Created by Greg on 23/01/2017.
  */
+// constantes
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
