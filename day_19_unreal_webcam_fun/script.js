e3b90a32015947a9636c9f08a72a537c2461cbc1
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-25 16:39:15 +0100

    adding red effect on canvas

--- day_19_unreal_webcam_fun/script.js
+++ day_19_unreal_webcam_fun/script.js
@@ -32,8 +32,15 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
-        const pixels = ctx.getImageData(0, 0, width, height);
-        console.log(pixels);
+        // take pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // create effect with pixels
+        pixels = redEffect(pixels);
+
+        // putting pixels back
+        ctx.putImageData(pixels, 0, 0);
+        // console.log(pixels);
     }, 16);
 }
 
@@ -55,6 +62,14 @@ function takePhoto() {
     // console.log(data);
 }
 
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i] = pixels.data[i + 0] + 100;        // R
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;    // G
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;   // B
+    }
+    return pixels
+}
 getVideo();
 
 // webcam video is displayed into canvas when canplay event is send by getVideo()
