5e2866a491db625ab42e4922e6e676cc79deb557
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-13 16:19:39 +0100

    setting constantes and variables

--- day_20_native_speech_recognition/script.js
+++ day_20_native_speech_recognition/script.js
@@ -1,3 +1,12 @@
 /**
  * Created by Greg on 13/02/2017.
  */
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const wordds = document.querySelector('.words');
+
+words.appendChild(p);
