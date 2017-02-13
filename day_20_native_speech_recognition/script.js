f67a6a0c97f7fdecf5ff0be5fb32ef536e626cc8
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-13 16:58:58 +0100

    setting npm server and start it in order to test speech recognition into console

--- day_20_native_speech_recognition/script.js
+++ day_20_native_speech_recognition/script.js
@@ -3,10 +3,19 @@
  */
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+// setting voice recognition
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
+recognition.lang = 'fr-FR';
 
+// setting constantes and variables to deal with DOM i.e add 'p' after class .words
 let p = document.createElement('p');
-const wordds = document.querySelector('.words');
+const words = document.querySelector('.words');
 
 words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    console.log(e);
+});
+
+recognition.start();
