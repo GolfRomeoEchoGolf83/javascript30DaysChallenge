e3d78d1341332d9469a8712ad52f4efa642dd92a
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-22 15:57:48 +0100

    make the arrow rotating when user's moving

--- day_21_geolocalisation_based_speedometer_and_compass/script.js
+++ day_21_geolocalisation_based_speedometer_and_compass/script.js
@@ -8,5 +8,15 @@ const speed = document.querySelector('.speed-value');
 // get speed data from device simulator using XCode
 navigator.geolocation.watchPosition((data) => {
     console.log(data);
+
+    // getting speed according to speed datas
+    speed.textContent = data.coords.speed;
+
+    /* getting integer value instead of float
     speed.textContent = Math.round(data.coords.speed);
+     */
+
+    // rotate compass according to heading datas
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 });
+
