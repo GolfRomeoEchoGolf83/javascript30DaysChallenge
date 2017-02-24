ccbf94dfe28682516b5207420433114497de2805
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-24 15:38:01 +0100

    creating highlight node into the DOM to visually trigger link moused over

--- day_22_follow_along_links/script.js
+++ day_22_follow_along_links/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Greg on 24/02/2017.
  */
+
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+
+// creating highlight
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
