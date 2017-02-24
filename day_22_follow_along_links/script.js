8eb05d760d8c86a0e1bf2db3b7deb7e35d618936
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-24 15:45:11 +0100

    testing the highlight triggering

--- day_22_follow_along_links/script.js
+++ day_22_follow_along_links/script.js
@@ -8,3 +8,10 @@ const highlight = document.createElement('span');
 // creating highlight
 highlight.classList.add('highlight');
 document.body.appendChild(highlight);
+
+// highlighting links
+function highlightLinks() {
+    console.log(this);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLinks));
