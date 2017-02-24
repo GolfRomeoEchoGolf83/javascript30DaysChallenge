d099ea8c96792c06d12cdb63ca2a727f31739dce
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-24 16:08:38 +0100

    highlighting links in the DOM when mouse over

--- day_22_follow_along_links/script.js
+++ day_22_follow_along_links/script.js
@@ -16,6 +16,7 @@ function highlightLinks() {
     console.log(linkCoords);
     highlight.style.width = `${linkCoords.width}px`;
     highlight.style.height = `${linkCoords.height}px`;
+    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLinks));
