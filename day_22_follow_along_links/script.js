cf400df4e6883acb3d63c8a91ef2ee46068bcf3c
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-24 16:19:19 +0100

    correcting scrolling problem with own coordonates

--- day_22_follow_along_links/script.js
+++ day_22_follow_along_links/script.js
@@ -14,9 +14,18 @@ function highlightLinks() {
     // where on the page trigger is happening
     const linkCoords = this.getBoundingClientRect();
     console.log(linkCoords);
-    highlight.style.width = `${linkCoords.width}px`;
-    highlight.style.height = `${linkCoords.height}px`;
-    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+
+    // changing coords to deal with Y issue when scrolling the window
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLinks));
