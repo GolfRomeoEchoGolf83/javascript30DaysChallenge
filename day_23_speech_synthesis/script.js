7a512f0fa84b287af60af4f1bab5e603fb4b92c0
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-03-05 22:23:42 +0100

    getting voices and putting it into dropdown

--- day_23_speech_synthesis/script.js
+++ day_23_speech_synthesis/script.js
@@ -6,4 +6,14 @@ const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 
-msg.text = document.querySelector('[name="text]').value;
+msg.text = document.querySelector('[name="text"]').value;
+
+function populatevoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+
+}
+
+speechSynthesis.addEventListener('voiceschanged', populatevoices);
