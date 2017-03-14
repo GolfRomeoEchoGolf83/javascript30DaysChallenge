c8f0c3909c6d20b4c416f72bb4cdcabca0f7b199
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-03-14 15:57:55 +0100

    fixing navbar when hitting top of the window

--- day_24_sticky_nav/script.js
+++ day_24_sticky_nav/script.js
@@ -4,7 +4,13 @@ const topOfNav = nav.offsetTop;
 
 // fixing navbar top of the window
 function fixNav() {
-    console.log(topOfNav);
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav')
+    }
 }
 
 window.addEventListener('scroll', fixNav);
